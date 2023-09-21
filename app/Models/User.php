36e47dc1b7b4e88ba797a487d02d2b9f703e7d22
= <=>
2023-09-21 20:17:44 +0700

    devkh

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,6 +31,7 @@ class User extends Authenticatable implements MustVerifyEmail
         'country',
         'no_tlp',
         'type_user',
+        'persence',
         'level',
         'status',
         'sertifikat',
--- resources/views/user/submision/edit.blade.php
+++ resources/views/user/submision/edit.blade.php
@@ -61,7 +61,6 @@
                     <div class="form-group">
                       <label for="abstract_file">Abstract</label>
                       <input type="file" class="form-control mb-2 @error('abstract_file') is-invalid @enderror" id="abstract_file" name="abstract_file">
-                      <a href="{{ \Illuminate\Support\Facades\Storage::url($submission->abstract_file) }}" target="_blank">Link Abstract</a>
                       @error('abstract_file')
                       <div class="invalid-feedback">
                         {{ $message }}
@@ -71,11 +70,6 @@
                     <div class="form-group">
                       <label for="paper">Full Paper</label>
                       <input type="file" class="form-control mb-2 @error('paper') is-invalid @enderror" id="paper" name="paper">
-                      @if ($submission->paper !== "")
-                      <a href="{{ \Illuminate\Support\Facades\Storage::url($submission->paper) }}" target="_blank">Link Full Paper</a>
-                      @else
-                      <td>Not yet available</td>
-                      @endif
                       @error('paper')
                       <div class="invalid-feedback">
                         {{ $message }}
