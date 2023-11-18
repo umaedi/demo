061277d90b0c46be68fface90d327cd928a9671f
= <=>
2023-11-18 09:33:02 +0700

    devkh

--- app/Http/Controllers/User/LoaController.php
+++ app/Http/Controllers/User/LoaController.php
@@ -27,27 +27,29 @@ class LoaController extends Controller
     public function download($id)
     {
         $submission = $this->submission->find($id);
-        $image = imagecreatefromjpeg(public_path('loa/loa.jpg'));
+        // title
+        $title = wordwrap($submission->title);
+        if (strlen($title) <= 69) {
+            $image = imagecreatefromjpeg(public_path('loa/loa_1.jpg'));
+            $position_title = 730;
+            $position_presence = 965;
+        } elseif (strlen($title) <= 137) {
+            $image = imagecreatefromjpeg(public_path('loa/loa_2.jpg'));
+            $position_title = 720;
+            $position_presence = 1004;
+        } else {
+            $image = imagecreatefromjpeg(public_path('loa/loa_3.jpg'));
+            $position_title = 715;
+            $position_presence = 1041;
+        }
 
         $imageWidth = imagesx($image);
         $textColor = imagecolorallocate($image, 0, 0, 0);
 
-        // $color = imagecolorallocate($image, 51, 51, 102);
         $fontPath = public_path('fonts/Poppins-SemiBold.ttf');
-
-        $margin = '1514';
-        // The text to be centered
-        $title = $submission->title;
-
-        // Periksa apakah lebar teks melebihi batas kertas
-        if ($title > $margin) {
-            // Jika ya, tambahkan tag <br> untuk turun ke baris berikutnya
-            $title = wordwrap($title);
-        }
 
         $presensi = '"' . $submission->user->presence . '"';
 
-        // Get the size of the text bounding box
         $fontSize = 24;
         $textBoundingBox = imagettfbbox($fontSize, 0, $fontPath, $title);
         $textWidth = $textBoundingBox[2] - $textBoundingBox[0];
@@ -60,12 +62,12 @@ class LoaController extends Controller
         $presensi_of_image = ($imageWidth - $textWidth1) / 2;
 
         // Add the centered text to the image
-        imagettftext($image, $fontSize, 0, $title_of_image, 730, $textColor, $fontPath, $title);
+        imagettftext($image, $fontSize, 0, $title_of_image, $position_title, $textColor, $fontPath, $title);
 
-        imagettftext($image, $fontSize, 0, $presensi_of_image, 1060, $textColor, $fontPath, $presensi);
+        imagettftext($image, $fontSize, 0, $presensi_of_image, $position_presence, $textColor, $fontPath, $presensi);
 
         $authors = $submission->author;
-        imagettftext($image, 24, 0, 280, 600, $textColor, $fontPath, $authors);
+        imagettftext($image, 24, 0, 190, 600, $textColor, $fontPath, $authors);
 
         // Set the content type header and output the image
         header("Content-type: image/png");
