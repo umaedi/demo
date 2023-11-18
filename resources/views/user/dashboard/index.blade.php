c696a4a98ef394acc86a6eb27a4876a7fa386956
= <=>
2023-11-18 20:44:18 +0700

    devkh

--- resources/views/user/dashboard/index.blade.php
+++ resources/views/user/dashboard/index.blade.php
@@ -85,7 +85,7 @@
             </div>
           </div>
         </div>
-        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+        <div class="col-lg-3 col-md-6 col-sm-6 col-12 mb-3">
           <a href="/user/submission" style="text-decoration: none">
           <div class="card card-statistic-1">
             <div class="card-icon bg-primary">
