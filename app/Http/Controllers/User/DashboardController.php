dc60573a3162c7e61f8afc909449aebc2c8fdcba
= <=>
2023-11-30 07:40:13 +0700

    devkh

--- app/Http/Controllers/User/DashboardController.php
+++ app/Http/Controllers/User/DashboardController.php
@@ -5,22 +5,37 @@ namespace App\Http\Controllers\User;
 use App\Models\Submission;
 use Illuminate\Support\Str;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function index()
     {
         if (\request()->ajax()) {
-            if (auth()->user()->sertifikat !== null) {
-                $image = imagecreatefromjpeg(public_path('img/template_sertifikat.jpg'));
+            if (auth()->user()->sertifikat !== null && Auth::user()->status == '2') {
+
+                if (Auth()->user()->type_user == 'Presenter') {
+                    $image = imagecreatefromjpeg(public_path('template/sertifikat_presenter.jpg'));
+                } else {
+                    $image = imagecreatefromjpeg(public_path('template/sertifikat_participant.jpg'));
+                }
 
                 $imageWidth = imagesx($image);
                 $textColor = imagecolorallocate($image, 0, 0, 0);
 
-                $name = auth()->user()->name;
-                $sertifikat = auth()->user()->sertifikat;
+                $name = Auth()->user()->name;
+                $id_sertifikat = Auth()->user()->sertifikat;
+                $no_sertifikat = Auth()->user()->id;
 
-                $fontPath = public_path('fonts/arial.ttf');
+                if ($no_sertifikat < 10) {
+                    $no_sertifikat = '00' . $no_sertifikat;
+                } elseif ($no_sertifikat < 100) {
+                    $no_sertifikat = '0' . $no_sertifikat;
+                } else {
+                    $no_sertifikat = $no_sertifikat;
+                }
+
+                $fontPath = public_path('fonts/Poppins-SemiBold.ttf');
                 $fontSize = 86;
                 $textBoundingBox = imagettfbbox($fontSize, 0, $fontPath, $name);
                 $textWidth = $textBoundingBox[2] - $textBoundingBox[0];
@@ -28,9 +43,13 @@ class DashboardController extends Controller
                 // Calculate coordinates to center the text
                 $textX = ($imageWidth - $textWidth) / 2;
 
-                // Add the centered text to the image
-                imagettftext($image, $fontSize, 0, $textX, 890, $textColor, $fontPath, $name);
-                imagejpeg($image, "sertifikat/$sertifikat.jpg");
+                //no sertifikat
+                imagettftext($image, 36, 0, 1475, 807, $textColor, $fontPath, $no_sertifikat);
+
+                // Aname
+                imagettftext($image, $fontSize, 0, $textX,  1020, $textColor, $fontPath, $name);
+
+                imagejpeg($image, "sertifikat/$id_sertifikat.jpg");
                 imagedestroy($image);
             }
         }
